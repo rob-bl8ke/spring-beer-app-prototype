6dd09926b850be6e95f453285fe08c162cbf3352
Rob Blake <rob.bl8ke@outlook.com>
2024-11-07 15:46:32 +0200

    PUT a beer

--- src/main/java/guru/springframework/spring_6_rest_api/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring_6_rest_api/controllers/BeerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +26,14 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
     
+
+    @PutMapping("{beerId}")
+    public ResponseEntity updatedById(@PathVariable("beerId") UUID beerId, @RequestBody Beer beer) {
+        beerService.updateBeerById(beerId, beer);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
     @PostMapping
     public ResponseEntity handlePost(@RequestBody Beer beer) {
         Beer savedBeer = beerService.saveNewBeer(beer);
--- src/main/java/guru/springframework/spring_6_rest_api/services/BeerService.java
+++ src/main/java/guru/springframework/spring_6_rest_api/services/BeerService.java
@@ -9,4 +9,5 @@ public interface BeerService {
     List<Beer> listBeers();
     Beer getBeerById(UUID id);
     Beer saveNewBeer(Beer beer);
+    void updateBeerById(UUID beerId, Beer beer);
 }
