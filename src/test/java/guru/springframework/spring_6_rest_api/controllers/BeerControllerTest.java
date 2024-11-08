710b3cfe4a7c1e6b3a483d9a610edf5705afdcf5
Rob Blake <rob.bl8ke@outlook.com>
2024-11-08 12:54:33 +0200

    Mock the beer service in a test and  return a pre-configured beer

--- src/test/java/guru/springframework/spring_6_rest_api/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring_6_rest_api/controllers/BeerControllerTest.java
@@ -1,21 +1,48 @@
 package guru.springframework.spring_6_rest_api.controllers;
 
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import guru.springframework.spring_6_rest_api.model.Beer;
+import guru.springframework.spring_6_rest_api.services.BeerService;
+import guru.springframework.spring_6_rest_api.services.BeerServiceImpl;
 
-import java.util.UUID;
+// These are all static imports
+// ... doesn't usually display in your intelisense list so must be added manually
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
 
+    // Create a mock of this service. By default all members return null.
+    @MockBean 
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+        // Go fetch a pre-configured beer
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+        
+        // Enhance the mocked service to return this pre-configured beer given any ID
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        
+        // Run a check to ensure the response is ok and the payload is sent in JSON format.
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+            .accept(org.springframework.http.MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(org.springframework.http.MediaType.APPLICATION_JSON))
+        ;
     }
 }
