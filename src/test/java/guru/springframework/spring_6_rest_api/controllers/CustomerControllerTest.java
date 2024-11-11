5564b403f94df59b59b8286ec561d7778dccfb79
Rob Blake <rob.bl8ke@outlook.com>
2024-11-11 07:48:22 +0200

    Test the customer controller get endpoints

---
+++ src/test/java/guru/springframework/spring_6_rest_api/controllers/CustomerControllerTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring_6_rest_api.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import guru.springframework.spring_6_rest_api.model.Customer;
+import guru.springframework.spring_6_rest_api.services.CustomerService;
+import guru.springframework.spring_6_rest_api.services.CustomerServiceImpl;
+
+// These are all static imports
+// ... doesn't usually display in your intelisense list so must be added manually
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
+    @Test
+    void testListCustomers() throws Exception {
+        given(customerService.listCustomers()).willReturn(customerServiceImpl.listCustomers());
+
+        mockMvc.perform(get("/api/v1/customer").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void getBeerById() throws Exception {
+        Customer testCustomer = customerServiceImpl.listCustomers().get(0);
+
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getId()).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())))
+            .andExpect(jsonPath("$.name", is(testCustomer.getName().toString())));
+
+    }
+}
