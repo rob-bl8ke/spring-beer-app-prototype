93ddc88097bb53760b55939ad502e87eb1ea1177
Rob Blake <rob.bl8ke@outlook.com>
2024-11-12 18:37:51 +0200

    Write an integration test (will transactional and rollback annotations) to keep the test localized

---
+++ src/test/java/guru/springframework/spring_6_rest_api/controllers/BeerControllerIntegationTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring_6_rest_api.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import guru.springframework.spring_6_rest_api.model.BeerDTO;
+import guru.springframework.spring_6_rest_api.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+
+// Load the entire context as this is an integration test
+@SpringBootTest
+public class BeerControllerIntegationTest {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
